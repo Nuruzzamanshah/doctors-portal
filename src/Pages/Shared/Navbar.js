6ac6ed922ac22c7db3d15314c4f145c1f0254dac
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-05-10 17:23:14 +0600

    text added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const menuItems = <>
-        <li><a>Home</a></li>
-        <li><a>About</a></li>
-        <li><a>Appointment</a></li>
-        <li><a>Reviews</a></li>
-        <li><a>Contact Us</a></li>
-        <li><a>Login</a></li>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/abour">About</Link></li>
+        <li><Link to="/appointment">Appointment</Link></li>
+        <li><Link to="/reviews">Reviews</Link></li>
+        <li><Link to="/contact us">Contact Us</Link></li>
+        <li><Link to="/login">Login</Link></li>
     </>
     return (
-        <div class="navbar bg-base-100">
-            <div class="navbar-start">
+        <div class="navbar bg-base-200">
+            <div class="navbar">
                 <div class="dropdown">
                     <label tabindex="0" class="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -27,9 +28,6 @@ const Navbar = () => {
                     {menuItems}
                     </ul>
                 </div>
-            <div class="navbar-end">
-                <a class="btn">Get started</a>
-            </div>
         </div>
     );
 };
