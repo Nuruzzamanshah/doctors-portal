cbd2f63ad103f3d3bd578a224409013ef3fad13b
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-05-10 22:17:51 +0600

    services added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Banner from './Banner';
 import Info from './Info';
+import Services from './Services';
+import SubBanner from './SubBanner';
 
 
 const Home = () => {
     return (
-        <div>
+        <div className='px-12'>
             <Banner></Banner>
             <Info></Info>
+            <Services></Services>
+            <SubBanner></SubBanner>
         </div>
     );
 };
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import InfoCard from './InfoCard';
 import clock from '../../assets/icons/clock.svg';
-import quote from '../../assets/icons/quote.svg';
+import marker from '../../assets/icons/marker.svg';
 import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={quote}></InfoCard>
-            <InfoCard img={phone}></InfoCard>
+            <InfoCard clore='bg-accent-content' cardSubTitle='Lorem Ipsum is simply dummy text of the pri' cardTitle='Opening Hours' img={clock}></InfoCard>
+            <InfoCard clore='bg-accent-focus' cardSubTitle='Brooklyn, NY 10036, United States' cardTitle='Visit our location' img={marker}></InfoCard>
+            <InfoCard clore='bg-success-content' cardSubTitle='01568235234' cardTitle='Contact us now' img={phone}></InfoCard>
         </div>
     );
 };
